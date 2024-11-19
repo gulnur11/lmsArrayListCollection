69c749b99986104b26a46e7027fcfac7ba4711cd
User <g3224686@gmail.com>
2024-11-19 14:03:11 +0600

    commit1111

--- src/Main.java
+++ src/Main.java
@@ -133,7 +133,7 @@ public class Main {
             }
         } catch (InputMismatchException e){
             System.out.println("Киргизилген маалымат сан эмес ! ");
-            System.out.println("test11111");
+            System.out.println("test1111111111");
         }finally {
                    System.out.println(" Operation is over!");
                }
